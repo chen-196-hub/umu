8d3abcb7bf95144ee06b949cc1d6b27d19894c1e
196 <kankou.chin@gmail.com>
2023-03-06 02:18:35 +0900

    feat: setup i18n

---
+++ lib/umu/core/language_setting.rb
@@ -0,0 +1,42 @@
+require 'yaml'
+require 'i18n'
+require_relative 'selector'
+require_relative '../beautifica/beautifica'
+
+I18n.load_path = Dir[File.join(File.dirname(__FILE__), '..', 'locales', '*.yml')]
+
+module Umu
+  # LanguageSetting is a module for setting language.
+  class LanguageSetting
+    LANG = {
+      en: 'English',
+      zh: '中文',
+      ja: '日本語'
+    }.freeze
+    extend Template
+
+    def self.setup_language
+      umu_lang_path = File.join(Dir.pwd, '.umu_lang')
+
+      save_language_choice(prompt_language) unless File.exist?(umu_lang_path)
+      umu_lang = YAML.load_file(umu_lang_path)
+      lang = umu_lang['language']
+      I18n.locale = lang
+      # puts I18n.t(:hello_world)
+    end
+
+    def self.prompt_language
+      logo
+
+      item = Umu::Selector.radio(LANG.values, 'Please select a language:')
+      LANG.key(item)
+    end
+
+    def self.save_language_choice(lang)
+      umu_lang_path = File.join(Dir.pwd, '.umu_lang')
+      umu_lang = {}
+      umu_lang['language'] = lang
+      File.write(umu_lang_path, umu_lang.to_yaml)
+    end
+  end
+end
